a580d275f9e5f1331ec65ebb530af21f69e9e595
Zhei-la <loopy9923@gmail.com>
2026-08-10 19:17:13 +0900

    글 보관 메모장 추가 (/memo)

---
+++ public/memo/sw.js
@@ -0,0 +1,30 @@
+/* 오프라인에서도 열리게 한다.
+   비행기 모드나 지하철에서도 저장해둔 글을 꺼내 쓸 수 있다. */
+const CACHE = 'geul-bogwan-v1';
+const FILES = ['./', './index.html', './manifest.json',
+               './icon-180.png', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+        return res;
+      })
+      .catch(() => caches.match(e.request).then(r => r || caches.match('./index.html')))
+  );
+});
