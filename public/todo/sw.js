f6ad0c0f25669ade3bd8a39227d3631ad890c069
Zhei-la <loopy9923@gmail.com>
2026-08-10 20:03:09 +0900

    메모장·할일 앱 추가 (/memo, /todo)

---
+++ public/todo/sw.js
@@ -0,0 +1,29 @@
+/* 오프라인에서도 열리게 한다. 인터넷이 없어도 할 일을 확인하고 체크할 수 있다. */
+const CACHE = 'halil-v1';
+const FILES = ['./', './index.html', './manifest.json',
+               './icon-180.png', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+        return res;
+      })
+      .catch(() => caches.match(e.request).then(r => r || caches.match('./index.html')))
+  );
+});
